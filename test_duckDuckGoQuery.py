155ef563540700a1e5b1e34a6ecb256c615e74f7
krmcgregor1 <krmcgregor1@my.waketech.edu>
2020-10-24 16:36:08 -0400

    Changed some names to match the results and made the query a global variable

--- test_duckDuckGoQuery.py
+++ test_duckDuckGoQuery.py
@@ -8,21 +8,18 @@ import pytest
 import requests
 
 
-
-@pytest.fixture
-def duckDuckGoQuery():
-    url = 'https://api.duckduckgo.com'
-    params = {'q':'presidents of the united states', 'format':'json'}
-    r = requests.get(url, params=params)
-    return r.text
+url = 'https://api.duckduckgo.com'
+params = {'q':'presidents of the united states.', 'format':'json'}
+r = requests.get(url, params=params)
+PRESIDENTDUMP = r.text
 
 
 
 @pytest.mark.parametrize('president_name', ['George Washington', 'John Adams', 'Thomas Jefferson', 'James Madison', 'James Monroe', 'John Quincy Adams', 'Andrew Jackson', 'Martin Van Buren', 
                                             'William Henry Harrison', 'John Tyler', 'James K. Polk', 'Zachary Taylor', 'Millard Fillmore', 'Franklin Pierce', 'James Buchanan', 'Abraham Lincoln',
-                                            'Andrew Johnson', 'Ulysses S. Grant', 'Rutherford B. Hayes', 'James Garfield', 'Chester A. Arthur', 'Grover Cleveland', 'Benjamin Harrison',
+                                            'Andrew Johnson', 'Ulysses S. Grant', 'Rutherford B. Hayes', 'James A. Garfield', 'Chester A. Arthur', 'Grover Cleveland', 'Benjamin Harrison',
                                             'Grover Cleveland', 'William McKinley', 'Theodore Roosevelt', 'William Howard Taft', 'Woodrow Wilson', 'Warren G. Harding', 'Calvin Coolidge',
-                                            'Herbert Hoover', 'Franklin D. Roosevelt', 'Harry S. Truman', 'Dwight D. Eisenhower', 'John F. Kennedy', 'Lyndon B. Johnson', 'Richard M. Nixon',
-                                            'Gerald R. Ford', 'James Carter', 'Ronald Reagan', 'George H. W. Bush', 'William J. Clinton', 'George W. Bush', 'Barack Obama', 'Donald J. Trump'])
-def test_president_names(duckDuckGoQuery, president_name):
-    assert president_name in duckDuckGoQuery
+                                            'Herbert Hoover', 'Franklin D. Roosevelt', 'Harry S. Truman', 'Dwight D. Eisenhower', 'John F. Kennedy', 'Lyndon B. Johnson', 'Richard Nixon',
+                                            'Gerald Ford', 'Jimmy Carter', 'Ronald Reagan', 'George H. W. Bush', 'Bill Clinton', 'George W. Bush', 'Barack Obama', 'Donald Trump'])
+def test_president_names(president_name):
+    assert president_name in PRESIDENTDUMP
